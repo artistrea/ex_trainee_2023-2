f68344bc1750be11fe649aa8e7a04f682f075929
artistrea <apiravesi@gmail.com>
2023-09-29 14:47:05 -0300

    next auth setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider, signIn } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function NavBar() {
+  return (
+    <a
+      style={{ cursor: "pointer" }}
+      onClick={(e) => {
+        e.preventDefault();
+        signIn("google");
+      }}
+    >
+      Entre na plataforma
+    </a>
+  );
+}
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <NavBar />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "../../../../prisma";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
