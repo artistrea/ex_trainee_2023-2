0e1eeea911398979e9c1244f568a519e6b7274e9
artistrea <apiravesi@gmail.com>
2023-09-29 14:54:40 -0300

    next auth funcionando

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "../../../../prisma";
+
+function googleCallback(profile: GoogleProfile) {
+  return profile.email_verified && profile.email.endsWith("@struct.unb.br");
+}
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -13,6 +17,14 @@ export const authOptions: NextAuthOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === "google") {
+        return googleCallback(profile as GoogleProfile);
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
+    },
+  },
 };
 
 export default NextAuth(authOptions);
